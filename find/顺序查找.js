eead01ef2fd43d6d44a099dae4cbdb3cff34ad8e
newbyvector <vectorse@126.com>
2018-08-15 22:14:56 +0800

    增加查找相关算法

---
+++ find/顺序查找.js
@@ -0,0 +1,22 @@
+//基于无序链表的顺序查找
+class Node {
+    constructor (key, value, next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    } 
+}
+class SequentialSearchST {
+    constructor() {
+        this.first = null;
+    }
+    equals(v1, v2) {
+        return v1 === v2;
+    }
+    get (key) {
+
+    }
+    put (key, value) {
+        
+    }
+}
